2efb72772897b8be99bc4775ce4527180204c74b
trinityXmontoya <trinityXmontoya@gmail.com>
2019-08-02 01:18:20 -0700

    working! return output to test again
    - print msgs
    - prerefactorr

--- src/computer_architecture/virtual_machine/prework/vm.clj
+++ src/computer_architecture/virtual_machine/prework/vm.clj
@@ -1,6 +1,19 @@
 (ns computer_architecture.virtual_machine.prework.vm)
 
 ;-------------
+;MAIN MEMORY
+;-------------
+(def main-memory (atom nil))
+
+(defn get-main-memory
+  [key]
+  (@main-memory key))
+
+(defn set-main-memory-output
+  [val]
+  (swap! main-memory assoc :output val))
+
+;-------------
 ;REGISTERS
 ;-------------
 (def program-counter-addr 0x00)
@@ -15,10 +28,12 @@
 
 (defn set-register
   [addr val]
+  (println "setting" addr val)
   (swap! registers assoc addr val))
 
 (defn get-register
   [addr]
+  (println "getting" addr (@registers addr))
   (@registers addr))
 
 ;-------------
@@ -49,21 +64,24 @@
 
 (defn parse-memory
   [memory]
-  (println "dat mem" memory)
   {:instructions (subvec memory instruction-start-index instruction-end-index)
    :output       (subvec memory output-start-index output-end-index)
    :input-1      (subvec memory input-1-start-index input-1-end-index)
    :input-2      (subvec memory input-2-start-index input-2-end-index)})
 
+(declare choose-and-convert-input)
 (defn load-word
   "Load value at given address into register"
-  [reg input]
-  (set-register reg input))
+  [reg addr]
+  (let [input (choose-and-convert-input addr)]
+    (set-register reg input)))
 
+(declare convert-output)
 (defn store-word
   "Store the value in register at the given address"
   [reg addr]
-  (get-register reg))
+  (let [output (convert-output (get-register reg))]
+    (set-main-memory-output output)))
 
 (defn add
   "Set reg1 = reg1 + reg2"
@@ -78,20 +96,23 @@
 ;-------------
 ;HELPER FNS
 ;-------------
-(defn third
-  [coll]
-  (nth coll 3))
-
 (defn convert-input
-  "given a collection of two little endian two-byte numbers we it to a base 10 number"
+  "given a collection of two little endian two-byte numbers convert to a base 10 number"
   [input]
+  (println "bout to be converted bb" input)
   (+ (first input) (* 256 (second input))))
 
+(defn convert-output
+  "given a base 10 number, convert it to a collection of two little endian two-byte numbers"
+  [output]
+  [(rem output 256) (quot output 256)])
+
 (defn choose-input
-  [input-idx input-1 input-2]
-  (if (= input-1-start-index input-idx)
-    input-1
-    input-2))
+  [input-idx]
+  (println "which input am1?" input-idx @main-memory)
+  (if (= input-idx input-1-start-index)
+    (@main-memory :input-1)
+    (@main-memory :input-2)))
 
 (def choose-and-convert-input
   (comp convert-input choose-input))
@@ -99,47 +120,46 @@
 ;-------------
 ;MAIN
 ;-------------
+; Clojure's data structures are immutable and it avoids state change when possible.
+; I could've accomplished this in a more functional, Clojure-y way by passing the vars that need to be updated
+; (main memory, register) as args to the loop and set them on each pass but i thought using [atoms](https://clojure.org/reference/atoms)
+; would be clearer and in this case it's an appropriate application for them!
 (defn virtual-machine
   [memory]
-  (let [{:keys [instructions input-1 input-2]} (select-keys (parse-memory memory) [:instructions :input-1 :input-2])]
-  (loop []
-    (let [current-instruction-idx (get-register program-counter-addr)]
+  (reset! main-memory (parse-memory memory))
+  (let [instructions (get-main-memory :instructions)]
+    (println "dem instructions" instructions)
+    (loop []
+      (let [current-instruction-idx (get-register program-counter-addr)]
+        (println "CURRRENT INSTRUCTION" current-instruction-idx)
+        (if (nil? current-instruction-idx)
+          (get-main-memory :output)                         ; i know the instructions said not to return anything but i can't otherwise return the atom values
+          (let [current-instruction (nth instructions current-instruction-idx)
+                first-arg (nth instructions (inc current-instruction-idx) nil)
+                second-arg (nth instructions (+ 2 current-instruction-idx) nil)]
 
-    (if (nil? current-instruction-idx)
-      nil
+            (case (instruction-set current-instruction)
 
-      (let [ current-instruction (nth instructions current-instruction-idx)]
-        (println "mah turn" current-instruction)
+              :load_word  (do (println "loading word" first-arg second-arg)
+                              (load-word first-arg second-arg)
+                              (set-register program-counter-addr (+ current-instruction-idx 3))
+                              (recur))
 
-      (case (instruction-set current-instruction)
+              :store_word (do (println "storing word" first-arg second-arg)
+                              (store-word first-arg second-arg)
+                              (set-register program-counter-addr (+ current-instruction-idx 3))
+                              (recur))
 
-        :load_word  (do
-                      (println "loading word")
-                      (load-word (second instructions) (choose-and-convert-input (third instructions) input-1 input-2))
-                      (set-register program-counter-addr (+ current-instruction-idx 3))
-                      (recur))
+              :add        (do (println "adding" first-arg second-arg)
+                              (add first-arg second-arg)
+                              (set-register program-counter-addr (+ current-instruction-idx 3))
+                              (recur))
 
-        :store_word (do
-                      (println "storing word")
-                      (store-word (second instructions) (third instructions))
-                      (set-register program-counter-addr (+ current-instruction-idx 3))
-                      (recur))
+              :sub        (do (println "subbing" first-arg second-arg)
+                              (sub first-arg second-arg)
+                              (set-register program-counter-addr (+ current-instruction-idx 3))
+                              (recur))
 
-        :add        (do
-                      (println "adding")
-                      (add (second instructions) (third instructions))
-                      (set-register program-counter-addr (+ current-instruction-idx 3))
-                      (recur))
-
-        :sub        (do
-                      (println "subbing")
-                      (sub (second instructions) (third instructions))
-                      (set-register program-counter-addr (+ current-instruction-idx 3))
-                      (recur))
-
-        ; clojure doesn't have a break statement. i could directly call java's System/exit
-        ; but I think saying there are no more instructions is btr bc it's semantically correct
-        :halt       (do
-                      (println "halting")
-                      (set-register program-counter-addr nil)
-                      (recur)))))))))
+              :halt       (do (println "halting")
+                              (set-register program-counter-addr nil)
+                              (recur)))))))))
--- test/computer_architecture/virtual_machine/prework/vm_test.clj
+++ test/computer_architecture/virtual_machine/prework/vm_test.clj
@@ -3,16 +3,14 @@
             [computer_architecture.virtual_machine.prework.vm :refer :all]))
 
 (deftest virtual-machine-test
-  (are [output instructions] (= (virtual-machine instructions) output)
-                      5293 [
-                         0x01, 0x01, 0x10,
-                         0x01, 0x02, 0x12,
-                         0x03, 0x01, 0x02,
-                         0x02, 0x01, 0x0e,
-                         0xff,
-                         0x00,
-                         0x00, 0x00,
-                         0xa1, 0x14,
-                         0x0c, 0x00
-                         ]))
+  (are [output instructions] (= (virtual-machine instructions) (convert-output output))
+                      5293 [0x01, 0x01, 0x10,
+                            0x01, 0x02, 0x12,
+                            0x03, 0x01, 0x02,
+                            0x02, 0x01, 0x0e,
+                            0xff,
+                            0x00,
+                            0x00, 0x00,
+                            0xa1, 0x14,
+                            0x0c, 0x00]))
 
