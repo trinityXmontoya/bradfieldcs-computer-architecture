fb1c905ee03e860be23194c526a5429f6fc0e20a
trinityXmontoya <trinityXmontoya@gmail.com>
2019-08-02 15:04:51 -0700

    working + branch_if_equal 😎

--- src/computer_architecture/virtual_machine/prework/vm.clj
+++ src/computer_architecture/virtual_machine/prework/vm.clj
@@ -28,13 +28,42 @@
 
 (defn set-register
   [addr val]
-  (println "setting" addr val)
+  (println "setting register" addr " to " val)
   (swap! registers assoc addr val))
 
 (defn get-register
   [addr]
-  (println "getting" addr (@registers addr))
+  (println "getting register" addr " current val - " (@registers addr))
   (@registers addr))
+
+;-------------
+;HELPER FNS
+;-------------
+(defn reset-registers!
+  []
+  (reset-vals! registers { program-counter-addr 0
+                           register-1-addr nil
+                           register-2-addr nil }))
+
+(defn convert-input
+  "given a collection of two little endian two-byte numbers convert to a base 10 number"
+  [input]
+  (+ (first input) (* 256 (second input))))
+
+(defn convert-output
+  "given a base 10 number, convert it to a collection of two little endian two-byte numbers"
+  [output]
+  [(rem output 256) (quot output 256)])
+
+(declare input-1-start-index)
+(defn choose-input
+  [input-idx]
+  (if (= input-idx input-1-start-index)
+    (@main-memory :input-1)
+    (@main-memory :input-2)))
+
+(def choose-and-convert-input
+  (comp convert-input choose-input))
 
 ;-------------
 ;INSTRUCTIONS
@@ -44,9 +73,10 @@
     0x02 :store_word
     0x03 :add
     0x04 :sub
-    0xff :halt })
+    0xff :halt
+    0x17 :branch_if_equal})
 
-(def instruction-len 14)
+(def instruction-len 18)
 (def instruction-start-index 0)
 (def instruction-end-index (+ instruction-start-index instruction-len))
 
@@ -69,97 +99,73 @@
    :input-1      (subvec memory input-1-start-index input-1-end-index)
    :input-2      (subvec memory input-2-start-index input-2-end-index)})
 
-(declare choose-and-convert-input)
 (defn load-word
   "Load value at given address into register"
-  [reg addr]
-  (let [input (choose-and-convert-input addr)]
+  [args]
+  (let [[reg addr] args
+        input (choose-and-convert-input addr)]
     (set-register reg input)))
 
-(declare convert-output)
 (defn store-word
   "Store the value in register at the given address"
-  [reg addr]
-  (let [output (convert-output (get-register reg))]
+  [args]
+  (let [[reg addr] args
+        output (convert-output (get-register reg))]
     (set-main-memory-output output)))
 
 (defn add
   "Set reg1 = reg1 + reg2"
-  [reg1 reg2]
-  (set-register reg1 (+ (get-register reg1) (get-register reg2))))
+  [args]
+  (let [[reg1 reg2] args]
+    (set-register reg1 (+ (get-register reg1) (get-register reg2)))))
 
 (defn sub
   "Set reg1 = reg1 - reg2"
-  [reg1 reg2]
-  (set-register reg1 (- (get-register reg1) (get-register reg2))))
+  [args]
+  (let [[reg1 reg2] args]
+    (set-register reg1 (- (get-register reg1) (get-register reg2)))))
 
-;-------------
-;HELPER FNS
-;-------------
-(defn convert-input
-  "given a collection of two little endian two-byte numbers convert to a base 10 number"
-  [input]
-  (println "bout to be converted bb" input)
-  (+ (first input) (* 256 (second input))))
+(defn halt
+  "sets the program counter to nil"
+  []
+  (set-register program-counter-addr nil))
 
-(defn convert-output
-  "given a base 10 number, convert it to a collection of two little endian two-byte numbers"
-  [output]
-  [(rem output 256) (quot output 256)])
-
-(defn choose-input
-  [input-idx]
-  (println "which input am1?" input-idx @main-memory)
-  (if (= input-idx input-1-start-index)
-    (@main-memory :input-1)
-    (@main-memory :input-2)))
-
-(def choose-and-convert-input
-  (comp convert-input choose-input))
+(defn branch-if-equal
+  "changes the program counter to the given address if the register values are equal"
+  [args]
+  (let [[reg1 reg2 addr] args]
+    (when (= reg1 reg2)
+      (set-register program-counter-addr addr))))
 
 ;-------------
 ;MAIN
 ;-------------
-; Clojure's data structures are immutable and it avoids state change when possible.
+(defn execute-and-increment
+  "sets the program counter to the next instruction, then performs the specified fn with the given arguments"
+  [fn current-instruction-idx amt-to-increment & args]
+  (set-register program-counter-addr (+ current-instruction-idx amt-to-increment))
+  (fn args))
+
 ; I could've accomplished this in a more functional, Clojure-y way by passing the vars that need to be updated
 ; (main memory, register) as args to the loop and set them on each pass but i thought using [atoms](https://clojure.org/reference/atoms)
 ; would be clearer and in this case it's an appropriate application for them!
 (defn virtual-machine
   [memory]
+  ; SETUP
+  (reset-registers!)
   (reset! main-memory (parse-memory memory))
-  (let [instructions (get-main-memory :instructions)]
-    (println "dem instructions" instructions)
-    (loop []
-      (let [current-instruction-idx (get-register program-counter-addr)]
-        (println "CURRRENT INSTRUCTION" current-instruction-idx)
-        (if (nil? current-instruction-idx)
-          (get-main-memory :output)                         ; i know the instructions said not to return anything but i can't otherwise return the atom values
+  ; RUN
+  (while (not (nil? (get-register program-counter-addr)))
+      (let [instructions (get-main-memory :instructions)
+            current-instruction-idx (get-register program-counter-addr)]
           (let [current-instruction (nth instructions current-instruction-idx)
-                first-arg (nth instructions (inc current-instruction-idx) nil)
-                second-arg (nth instructions (+ 2 current-instruction-idx) nil)]
-
+                first-arg           (nth instructions (inc current-instruction-idx) nil)
+                second-arg          (nth instructions (+ 2 current-instruction-idx) nil)
+                third-arg           (nth instructions (+ 3 current-instruction-idx) nil)]
             (case (instruction-set current-instruction)
-
-              :load_word  (do (println "loading word" first-arg second-arg)
-                              (load-word first-arg second-arg)
-                              (set-register program-counter-addr (+ current-instruction-idx 3))
-                              (recur))
-
-              :store_word (do (println "storing word" first-arg second-arg)
-                              (store-word first-arg second-arg)
-                              (set-register program-counter-addr (+ current-instruction-idx 3))
-                              (recur))
-
-              :add        (do (println "adding" first-arg second-arg)
-                              (add first-arg second-arg)
-                              (set-register program-counter-addr (+ current-instruction-idx 3))
-                              (recur))
-
-              :sub        (do (println "subbing" first-arg second-arg)
-                              (sub first-arg second-arg)
-                              (set-register program-counter-addr (+ current-instruction-idx 3))
-                              (recur))
-
-              :halt       (do (println "halting")
-                              (set-register program-counter-addr nil)
-                              (recur)))))))))
+              :load_word       (execute-and-increment load-word       current-instruction-idx 3 first-arg second-arg)
+              :store_word      (execute-and-increment store-word      current-instruction-idx 3 first-arg second-arg)
+              :add             (execute-and-increment add             current-instruction-idx 3 first-arg second-arg)
+              :sub             (execute-and-increment sub             current-instruction-idx 3 first-arg second-arg)
+              :branch_if_equal (execute-and-increment branch-if-equal current-instruction-idx 4 first-arg second-arg third-arg)
+              :halt            (halt))))))
--- test/computer_architecture/virtual_machine/prework/vm_test.clj
+++ test/computer_architecture/virtual_machine/prework/vm_test.clj
@@ -3,14 +3,38 @@
             [computer_architecture.virtual_machine.prework.vm :refer :all]))
 
 (deftest virtual-machine-test
-  (are [output instructions] (= (virtual-machine instructions) (convert-output output))
-                      5293 [0x01, 0x01, 0x10,
-                            0x01, 0x02, 0x12,
-                            0x03, 0x01, 0x02,
-                            0x02, 0x01, 0x0e,
-                            0xff,
-                            0x00,
-                            0x00, 0x00,
-                            0xa1, 0x14,
-                            0x0c, 0x00]))
+  (are [output instructions] (= (do (virtual-machine instructions)
+                                    (@main-memory :output))
+                                (convert-output output))
+                      5293 [0x01, 0x01, 0x14,               ; instruction 0 - set reg1 to input 1 (5281)
+                            0x01, 0x02, 0x16,               ; instruction 3 - set reg2 to input 2 (12)
+                            0x03, 0x01, 0x02,               ; instruction 6 - reg1 = reg1 + reg2
+                            0x02, 0x01, 0x12,               ; instruction 9 - store val in reg1 (5293) at output
+                            0xff,                           ; instruction 12 - halt
+                            0x00, 0x00, 0x00, 0x00, 0x00,   ; unused instruction space
+                            0x00, 0x00,                     ; output
+                            0xa1, 0x14,                     ; input 1 (5281)
+                            0x0c, 0x00]                     ; input 2 (12)
+
+
+                      10562 [0x01, 0x01, 0x14,              ; instruction 0 - set reg1 to input 1 (5281)
+                             0x01, 0x02, 0x14,              ; instruction 3 - set reg2 to input 1 (5281)
+                             0x03, 0x01, 0x02,              ; instruction 6 - reg1 = reg1 + reg2
+                             0x02, 0x01, 0x12,              ; instruction 9 - store val in reg1 (10562) at output
+                             0xff,                          ; instruction 12 - halt
+                             0x00, 0x00, 0x00, 0x00, 0x00,  ; unused instruction space
+                             0x00, 0x00,                    ; output
+                             0xa1, 0x14,                    ; input 1 (5281)
+                             0x0c, 0x00]                    ; input 2 (12)
+
+                      5281 [0x01, 0x01, 0x14,               ; instruction 0 - set reg1 to input 1 (5281)
+                            0x17, 0x01, 0x01, 0xa,          ; instruction 3 - reg1 = reg1, so jump to instruction 10
+                            0x01, 0x02, 0x16,               ; instruction 7 - set reg2 to input 2 (12)
+                            0x02, 0x01, 0x12,               ; instruction 10 - store val in reg1 (5281) at output
+                            0xff,                           ; instruction 13 - halt
+                            0x00, 0x00, 0x00, 0x00          ; unused instruction space
+                            0x00, 0x00,                     ; output
+                            0xa1, 0x14,                     ; input 1 (5281)
+                            0x0c, 0x00]                     ; input 2 (12)
+                            ))
 
